e4b546df5203bdff080da2c07b892d3eb60dc445
Dav <davide.filippi@coolshop.com>
2019-01-02 17:34:26 +0100

    new repository plus axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,60 @@
 import React, {Component} from 'react';
 import '../css/components/App.css';
+import axios from 'axios';
 import {Button} from 'react-bootstrap';
 
 import NewRep from './NewRepository';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {addRep: false};
+    }
+
+    toggleNewRepository = () => {
+        this.setState({addRep: !this.state.addRep});
+    };
+
+    newRepositoryShow = (showNewRep) => {
+        if (showNewRep) {
+            return <div className="ui container"><NewRep addRepository={this.toggleNewRepository}/></div>;
+        }
+
+        return <Button bsStyle="primary" onClick={this.toggleNewRepository}>Add repository</Button>;
+    };
+
+
     render() {
         return (
             <div className="App">
                 <header className="App-header">Money Save</header>
                 <div className="App-body">
-                    <Button bsStyle="primary">Add repository</Button>
-                    <NewRep/>
-                    <table className="rep-list">
-                        <thead>
-                        <tr>
-                            <th>Rep Name</th>
-                            <th>Rep total</th>
-                            <th>Actions</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr className="rep-element">
-                            <td>name</td>
-                            <td>total</td>
-                            <td>
-                                <Button bsStyle="danger">edit</Button>
-                                <Button bsStyle="secondary">cancel</Button>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
+                    {this.newRepositoryShow(this.state.addRep)}
+                    {/*<div className="rep-list">
+                        <table>
+                            <thead>
+                            <tr>
+                                <th>Rep Name</th>
+                                <th>Rep total</th>
+                                <th>Actions</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr className="rep-element">
+                                <td>name</td>
+                                <td>total</td>
+                                <td>
+                                    <div className="btn-group">
+                                        <Button bsStyle="danger">edit</Button>
+                                        <Button bsStyle="secondary">cancel</Button>
+                                    </div>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>*/}
                 </div>
             </div>
         );
--- src/components/NewRepository.js
+++ src/components/NewRepository.js
@@ -1,35 +1,47 @@
 import React, {Component} from 'react';
 import '../css/components/NewRepository.css';
-import {Button, Col, Grid, Row} from "react-bootstrap";
+import {Button, Row} from "react-bootstrap";
 
 
 class NewRepository extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.newRep = {name: 'undefined', startingValue: 0, type: 'undefined'};
+    }
+
+    cancelNewRepository = () => {
+        this.newRep = {name: 'undefined', startingValue: 0, type: 'undefined'};
+        this.props.addRepository();
+    };
+
+    addNewRepository = () => {
+        this.props.addRepository();
+    };
+
     render() {
         return (
-            <div className="new-repository">
-                <form>
-                    <Grid>
-                        <Row>
-                            <Col xs={4}>
-                                <Row>
-                                    <label htmlFor="name">Repository Name</label>
-                                    <input id="name" type="text"/>
-                                </Row>
-                                <Row>
-                                    <label htmlFor="startingValue">Starting Currency</label>
-                                    <input id="startingValue" type="text"/>
-                                </Row>
-                                <Row>
-                                    <label htmlFor="type">Type</label>
-                                    <input id="type" type="text"/>
-                                </Row>
-                                <Row>
-                                    <Button bsStyle="danger">Cancel</Button>
-                                    <Button bsStyle="primary">Confirm</Button>
-                                </Row>
-                            </Col>
-                        </Row>
-                    </Grid>
+            <div className="new-repository container-fluid">
+                <form onSubmit={this.addNewRepository}>
+                    <Row>
+                        <label htmlFor="name">Repository Name</label>
+                        <input id="name" type="text" placeholder={this.newRep.name}/>
+                    </Row>
+                    <Row>
+                        <label htmlFor="startingValue">Starting Currency</label>
+                        <input id="startingValue" type="text" placeholder={this.newRep.startingValue}/>
+                    </Row>
+                    <Row>
+                        <label htmlFor="type">Type</label>
+                        <input id="type" type="text" placeholder={this.newRep.type}/>
+                    </Row>
+                    <Row>
+                        <div className="btn-group">
+                            <Button bsStyle="danger" onClick={this.cancelNewRepository}>Cancel</Button>
+                            <Button bsStyle="primary" onClick={this.addNewRepository}>Confirm</Button>
+                        </div>
+                    </Row>
                 </form>
             </div>
         );
